1288da69bffce8e76661cac8c55e64db40ab5031
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2024-05-10 14:31:58 +0300

    NetworkNode

--- network/models.py
+++ network/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class NetworkNode(models.Model):
+    """Модель торговой сети"""
+
+    LEVEL_CHOICES = (
+        ('0', 'Завод'),
+        ('1', 'Розничная сеть'),
+        ('2', 'Индивидуальный предприниматель'),
+    )
+
+    name = models.CharField(max_length=255, verbose_name='Название')
+
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    country = models.CharField(max_length=100, verbose_name='Страна')
+    city = models.CharField(max_length=150, verbose_name='Город')
+    street = models.CharField(max_length=150, verbose_name='Улица')
+    house_number = models.CharField(max_length=10, verbose_name='Номер дома')
+
+    products = models.ManyToManyField(Product, related_name='network_nodes', verbose_name='Продукты')
+
+    supplier = models.ForeignKey('self', on_delete=models.SET_NULL, **NULLABLE, verbose_name='Поставщик')
+    level = models.IntegerField(choices=LEVEL_CHOICES, **NULLABLE)
+    debt = models.DecimalField(max_digits=20, decimal_places=2, verbose_name='Задолженность')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f'{self.name}, {self.email}, {self.country}'
+
+    def save(self, *args, **kwargs):
+        if self.supplier:
+            self.level = self.supplier.level + 1
+        else:
+            self.level = 0
+        super(NetworkNode, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Звено сети'
+        verbose_name_plural = 'Звенья сети'
+        ordering = ('id',)
