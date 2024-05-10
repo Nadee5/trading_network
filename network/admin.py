534db37566193d925ce5ee6578bd24d154c6ef62
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2024-05-10 21:47:13 +0300

    admin

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from network.models import NetworkNode
+
+
+@admin.register(NetworkNode)
+class NetworkNodeAdmin(admin.ModelAdmin):
+    exclude = ('level',)
+    list_display = ('pk', 'name', 'email', 'country', 'supplier', 'level', 'debt',)
+    list_filter = ('country', 'city',)
+    search_fields = ('name', 'country', 'supplier',)
+    ordering = ('pk', 'name', 'debt',)
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'model', 'release_date',)
+    search_fields = ('title', 'model',)
+    ordering = ('release_date',)
