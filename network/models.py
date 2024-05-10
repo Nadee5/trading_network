2c083127888260f417ab4352be4207ab5f55924a
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2024-05-10 21:47:39 +0300

    changes in NetworkNode

--- network/models.py
+++ network/models.py
@@ -9,9 +9,9 @@ class NetworkNode(models.Model):
     """Модель торговой сети"""
 
     LEVEL_CHOICES = (
-        ('0', 'Завод'),
-        ('1', 'Розничная сеть'),
-        ('2', 'Индивидуальный предприниматель'),
+        (0, 'Завод'),
+        (1, 'Розничная сеть'),
+        (2, 'Индивидуальный предприниматель'),
     )
 
     name = models.CharField(max_length=255, verbose_name='Название')
@@ -25,7 +25,7 @@ class NetworkNode(models.Model):
     products = models.ManyToManyField(Product, related_name='network_nodes', verbose_name='Продукты')
 
     supplier = models.ForeignKey('self', on_delete=models.SET_NULL, **NULLABLE, verbose_name='Поставщик')
-    level = models.IntegerField(choices=LEVEL_CHOICES, **NULLABLE)
+    level = models.IntegerField(choices=LEVEL_CHOICES, **NULLABLE, verbose_name='Уровень звена')
     debt = models.DecimalField(max_digits=20, decimal_places=2, verbose_name='Задолженность')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
@@ -33,9 +33,14 @@ class NetworkNode(models.Model):
         return f'{self.name}, {self.email}, {self.country}'
 
     def save(self, *args, **kwargs):
+        """Переопределение метода для автоматической привязки уровня иерархии звена."""
         if self.supplier:
+            # Если у поставщика объекта уровень 2, то уровень сохраняется и для текущего звена (max level)
+            if self.supplier.level == 2:
+                self.level = 2
             self.level = self.supplier.level + 1
         else:
+            # Если у объекта нет поставщика, он считается заводом (уровень 0)
             self.level = 0
         super(NetworkNode, self).save(*args, **kwargs)
 
