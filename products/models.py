d6c4aca525a93b9189149d48bddc3b0d6bf4492f
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2024-05-10 13:49:47 +0300

    Product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Название')
+    model = models.CharField(max_length=150, verbose_name='Модель')
+    release_date = models.DateField(verbose_name='Дата выхода на рынок.')
+
+    def __str__(self):
+        return f'{self.title} {self.model}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
